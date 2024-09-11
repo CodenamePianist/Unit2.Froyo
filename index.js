2f3b6b44b6e5c0a6c0641899294ecd80facce152
Brandon Hamilton <bham2015@gmail.com>
2024-09-11 18:00:03 -0500

    The changes in this commit will add the starting pseudocode for this program.

---
+++ index.js
@@ -0,0 +1,5 @@
+// User inputs their flavors into a prompt.
+// Entry is converted into a string.
+// This string of answers is then split into an array of strings.
+// This array will be added to an object. In this object the key-value pair is the flavor and how many times it has come up.
+// Organize the logic into a function.
