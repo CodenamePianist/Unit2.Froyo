ec108ee3cd1e2981f6d92eab93381abb0302c71c
Brandon Hamilton <bham2015@gmail.com>
2024-09-11 23:34:28 -0500

    The changes in this commit will add the logic to create the object and put it all in a function.

--- index.js
+++ index.js
@@ -7,6 +7,30 @@
 
 const flavorInput = prompt("What flavors would you like?", "vanilla,vanilla,vanilla,strawberry,coffee,coffee");
 
-const stringArray = flavorInput.split(",");
+const flavorArray = flavorInput.split(",");
 
-console.log(stringArray);
+
+/**
+ * 
+ * @param {array} userFlavors The flavors the user put in the prompt 
+ * @returns {object} The flavors and how many times they were entered
+ */
+function getFlavors(userFlavors) {
+    // Creating an empty body named flavors
+    const flavors = {};
+    // This for loop iterates thru the array in flavorArray
+    for (let i = 0; i < userFlavors.length; i++) {
+        // At each iteration, the flavor is stored in the variable flavor
+        let flavor = userFlavors[i];
+        // If a flavor is already in the object, its value is increased by 1
+        if (flavor in flavors) {
+            flavors[flavor] += 1;
+            // Otherwise, the flavor is added to the object and it is given a value of 1
+        } else {
+            flavors[flavor] = 1;
+        }
+    }
+    return flavors;
+}
+
+console.log(getFlavors(flavorArray));
